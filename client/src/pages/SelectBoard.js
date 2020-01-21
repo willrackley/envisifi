7f92a5a4d64e947459e9b51183a3886ef9b80cc4
williamrackley <willrackley65@gmail.com>
2020-01-21 14:53:15 -0500

    got functionality to upload and download photo to your phone

--- client/src/pages/SelectBoard.js
+++ client/src/pages/SelectBoard.js
@@ -3,10 +3,9 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav"
 import NavDropdown from "react-bootstrap/NavDropdown"
 import Jumbotron from "react-bootstrap/Jumbotron"
+import Spinner from "react-bootstrap/Spinner"
 import Button from "react-bootstrap/Button"
 import { GiSmartphone} from "react-icons/gi";
-import { GiTablet} from "react-icons/gi";
-import { GiPc} from "react-icons/gi";
 import Cropper from 'cropperjs';
 import 'cropperjs/dist/cropper.css';
 import html2canvas from 'html2canvas';
@@ -15,16 +14,15 @@ import "./style.css";
 
 class selectBoard extends Component {
     state = {
-        deviceTypeButtonDiv: "deviceType",
+        deviceTypeButtonDiv: "PHONE",
         editSection: "none",
         phoneOutlinePortrait: <div></div>,
         placeHolder1Style: { backgroundImage:  "url('http://www.eco-trailer.co.uk/wp-content/uploads/2016/03/placeholder-blank.jpg')"},
         divImagePlaceholder1: "url('http://www.eco-trailer.co.uk/wp-content/uploads/2016/03/placeholder-blank.jpg')",
         clickedPlaceholder: 0,
-        selectedFile: "",
-        selectedRawFile: null,
-        imageUploaded: false,
-        test: "10px solid black"
+        size: null,
+        base64Download: "",
+        isLoading: false
     }
     
     
@@ -33,19 +31,14 @@ class selectBoard extends Component {
     }
 
     selectImgFile = event => {
-        // this.setState({ selectedFile: URL.createObjectURL(event.target.files[0]) });
-        // this.setState({ selectedRawFile: event.target.files[0]})
-        // this.setState({ imageUploaded: true });
-        
-        const file = event.target.files[0];
-       
-        // const preview1 = document.getElementById('placeholder1');
-        // const preview2 = document.getElementById('placeholder2');
 
+        const file = event.target.files[0];
+        
         for (let i=1; i <= 5; i++) {
             
             if (this.state.clickedPlaceholder === i) {
                 const reader = new FileReader();
+                
                 reader.addEventListener("load", () => {
                     // remove any cropper if user decides to change image
                     let addedImg = document.getElementsByClassName(`halfSizePlaceholder${i}`)
@@ -56,25 +49,22 @@ class selectBoard extends Component {
                     document.getElementById(`placeholder${i}`).src = reader.result;
                     let cropper = new Cropper(document.getElementById(`placeholder${i}`), {dragMode: "move", guides: false, background: false, viewMode: 0, autoCropArea: 0, cropBoxResizable: false,cropBoxMovable: false, autoCrop: true, modal: false, center: false, highlight: false })
                     cropper.replace(document.getElementById(`placeholder${i}`).src)
-    
-                
+              
                 }, false);
                 reader.readAsDataURL(file);
             }
-        }
-        
-        if (file) {
-            
-            
-        }
+        } 
     }
 
-    handleDeviceEditSection = (device, width, height)=> {
-        let size = { device: {device: device, width: width, height: height} };
-        console.log(size);
+    handleDeviceEditSection = async (device, width, height)=> {
+
+        await this.setState({size: { device: {device: device, width: width, height: height} }, phoneOutlinePortrait: <div></div>, isLoading: true });
+
+        await new Promise((resolve, reject) => setTimeout(resolve, 500));
+
         if (this.state.deviceTypeButtonDiv === "iphoneTypes"){
             this.setState({ phoneOutlinePortrait: 
-                <div className="mx-auto mb-5 phoneScreen" style={{width: "200px", height: "300px", border: "8px solid #a9a9a9", background: "#F3F3F3", borderRadius: "25px", overflow: "hidden"}}> 
+                <div className="mx-auto phoneScreen" style={{width: this.state.size.device.width/2, height: this.state.size.device.height/2, background: "#F3F3F3",  overflow: "hidden", display: "block"}}> 
                 
                     <div className="halfSizePlaceholder1" onDoubleClick={()=>{document.getElementById('fileInput1').click(); this.setState({ clickedPlaceholder: 1})}} >
                         <img id="placeholder1" src='http://www.eco-trailer.co.uk/wp-content/uploads/2016/03/placeholder-blank.jpg' alt="placeholder"/>
@@ -83,19 +73,49 @@ class selectBoard extends Component {
                     <div className="halfSizePlaceholder2" onDoubleClick={()=>{document.getElementById('fileInput2').click(); this.setState({ clickedPlaceholder: 2})}} >
                         <img id="placeholder2" src='http://www.eco-trailer.co.uk/wp-content/uploads/2016/03/placeholder-blank.jpg' alt="placeholder"/>
                     </div>
-              </div>
+              </div>,
+              isLoading: false
             })
         }
        
     }
 
-    screenshot = () => {
-        html2canvas(document.getElementsByClassName('phoneScreen')[0], {height: 500}).then(function(canvas) {
-        document.getElementsByClassName('screenshotSec')[0].appendChild(canvas);
-        
-       });
-    
+    screenshot = async () => {
+        let base64URL;
+        let imgdataURL;
+        let deviceHeight = this.state.size.device.height;
+        let deviceWidth = this.state.size.device.width;
+
+        await html2canvas(document.getElementsByClassName('phoneScreen')[0], {height: this.state.size.device.height/2, scrollX: 0, scrollY: -window.scrollY}).then(function(canvas) {
+       
+            document.getElementsByClassName('screenshotSec')[0].appendChild(canvas);
+
+            base64URL = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+
+            const img = new Image();
+            img.src = base64URL;
+            img.height = deviceHeight;
+            img.width = deviceWidth;
+            img.classList.add('increasedSize')
+            document.getElementsByClassName('largePic')[0].append(img)
+
+            img.onload = function() {
+                let newCanvas = document.createElement("canvas");
+                newCanvas.width = img.width;
+                newCanvas.height = img.height;
+                let ctx = newCanvas.getContext("2d");
+                ctx.drawImage(img, 0, 0, newCanvas.width, newCanvas.height);
+                imgdataURL = newCanvas.toDataURL("image/png").replace('image/png', 'image/octet-stream');
+            }
+        });
+
+      
+       await new Promise((resolve, reject) => setTimeout(resolve, 1000))
+       this.setState({ base64Download: imgdataURL })
+       
     }
+
+
 
     render() {
         let deviceButtons = <div></div>
@@ -106,8 +126,8 @@ class selectBoard extends Component {
 
                 <div className="text-center">
                     <Button className="m-2" onClick={() => this.handleDeviceButtonClick("PHONE")}> PHONE <br/><GiSmartphone size="10em"/> </Button>
-                    <Button className="m-2"> TABLET<br/> <GiTablet size="10em"/> </Button>
-                    <Button className="m-2"> COMPUTER <br/> <GiPc size="10em"/> </Button>
+                    {/* <Button className="m-2"> TABLET<br/> <GiTablet size="10em"/> </Button>
+                    <Button className="m-2"> COMPUTER <br/> <GiPc size="10em"/> </Button> */}
                 </div>
             </div>;
 
@@ -120,23 +140,40 @@ class selectBoard extends Component {
             deviceButtons = <div>
                                  <div className="text-center"> Which Type of Phone?</div>
                                  <div className="text-center">
-                                    <Button variant="none" className="m-2" style={{height: "12em", width: "8em", backgroundImage: "url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-purple-select-2019?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1566960958082)", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}} onClick={() => this.handleDeviceButtonClick("iphoneTypes")}> <span className="text-dark">iphone</span> </Button>
+                                    <Button 
+                                    variant="none" 
+                                    className="m-2" 
+                                    style={{height: "12em", width: "8em", backgroundImage: "url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-purple-select-2019?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1566960958082)", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}} 
+                                    onClick={() => this.handleDeviceButtonClick("iphoneTypes")}>
+                                        <span className="text-dark">iphone</span>
+                                    </Button>
 
                                     <Button variant="none" className="m-2 text-dark" style={{height: "12em", width: "8em", backgroundImage: "url(https://images.samsung.com/is/image/samsung/p5/ph/smartphones/ph-pcd-galaxy-a50.png?$ORIGIN_PNG$", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}> <span >SAMSUNG</span> </Button>
                                 </div>
                             </div>
         } else if (this.state.deviceTypeButtonDiv === "iphoneTypes") {
+            
             backButton = <Button onClick={() => this.handleDeviceButtonClick("PHONE")}>Back</Button>;
-            deviceButtons = <div className="text-center">
-                                <Button className="m-2" onClick={() => {this.handleDeviceEditSection("Xs Max", 1242, 2688)}}>Xs Max</Button>
 
-                                <Button className="m-2" onClick={() => {this.handleDeviceEditSection("11 Pro Max", 1242, 2688)}}>11 Pro Max</Button>
+            if(this.state.isLoading){
+                deviceButtons = <div className="text-center"><Spinner animation="border" /></div>
+            } else {
+                deviceButtons = <div className="text-center">
+                                <Button 
+                                className="m-2" 
+                                onClick={() => {this.handleDeviceEditSection("Xs Max", 414, 896)}}>
+                                    Xs Max
+                                </Button>
+
+                                <Button className="m-2" onClick={() => {this.handleDeviceEditSection("11 Pro Max", 414, 896)}}>11 Pro Max</Button>
 
 
                                 <Button className="m-2" onClick={() => {this.handleDeviceEditSection("11",828, 1792)}}>11</Button>
 
                                 <Button className="m-2" onClick={() => {this.handleDeviceEditSection("Xr", 828, 1792)}}>Xr</Button>
                             </div>
+            }
+            
         }
 
         
@@ -167,27 +204,28 @@ class selectBoard extends Component {
                     </div>
                     {deviceButtons}
                 </Jumbotron>
-                <div className="row">
-                    <div className="col-6">
-                        <div className="editSection">
-                            <div className="form-group">
-                                <input id="fileInput1" className="form-control text-center" type="file"
-                                onChange={this.selectImgFile}
-                                accept="image/*" style={{display: "none"}}/>
-                                <input id="fileInput2" className="form-control text-center" type="file"
-                                onChange={this.selectImgFile}
-                                accept="image/*" style={{display: "none"}}/>
-                            </div> 
-                            {this.state.phoneOutlinePortrait}
-                            <Button onClick={() => {this.screenshot()}}>download</Button>
-                        </div>
-                    </div>
-                    <div className="col-6">
+
+                <div className="editSection">
+                    {this.state.phoneOutlinePortrait}   
+                    <div className="form-group">
+                        <input id="fileInput1" className="form-control text-center" type="file"
+                        onChange={this.selectImgFile}
+                        accept="image/*" style={{display: "none"}}/>
+                        <input id="fileInput2" className="form-control text-center" type="file"
+                        onChange={this.selectImgFile}
+                        accept="image/*" style={{display: "none"}}/>
+                    </div> 
+                            
+                    <Button onClick={() => {this.screenshot()}}>Done</Button>
+
+                    <a href={this.state.base64Download} download="newBoard.png"   >Download</a>
+                        
+                    <div>
                         <div className="screenshotSec"></div>
                     </div>
+               
+                    <div className="largePic"></div>
                 </div>
-                
-                
             </div>
         )
     }
