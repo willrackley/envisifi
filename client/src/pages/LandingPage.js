8c3959aefa0a511486d0875876d50b57d2e1da71
williamrackley <willrackley65@gmail.com>
2020-04-08 14:03:06 -0400

    added landing page graphic

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav"
+//import Nav from "react-bootstrap/Nav"
 //import NavDropdown from "react-bootstrap/NavDropdown"
 import Jumbotron from "react-bootstrap/Jumbotron"
 import Button from "react-bootstrap/Button"
@@ -24,30 +24,32 @@ class landingPage extends Component {
             <div>
                 <Navbar bg="light" expand="lg">
                     <Navbar.Brand className="navbar_title text-center w-100" href="#home">Envisifi</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">
-                            {/* <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link> */}
-                            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link> 
+                              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown> */}
+                            </NavDropdown>
                             </Nav>
-                    </Navbar.Collapse>
+                    </Navbar.Collapse> */}
                 </Navbar>
 
                 <Jumbotron id="landingPageJumbotron">
-                    
+                    <div className="">
+                        <p className="lpText text-center display-4 mt-4">
+                            Make a collage wallpaper to fit your iPhone perfectly!
+                        </p>
+                        <div className="text-center mt-5">
+                            <Button variant="info" id="getStartedBtn" onClick={() => this.handleGetStartedButton()}>Get Started</Button>
+                        </div>
+                    </div>
                 </Jumbotron>
-
-                <div>
-                <Button variant="info" onClick={() => this.handleGetStartedButton()}>Get Started</Button>
-                {/* <NavLink to="/selectBoard"> Get Started </NavLink> */}
-                </div>
             </div>
         )
     }
